0afb54cd1eb0940abe0fcc7ccd9322c87f41d2b5
JannickL <jannick.leismann@gmx.de>
2022-08-09 13:40:47 +0200

    added functionality of adding, deleting and toggleing a task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ]
+
+  add(newTask: string){
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(exisitingTask: Task){
+    var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${exisitingTask.title}"`)
+  
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task => task != exisitingTask);
+    }
+  
+  }
+}
+
+class Task {
+
+  constructor(public title: string){
+
+  }
+
+  toggleIsDone(){
+    this.isDone = !this.isDone;
+  }
+
+  public isDone = false;
+
 }
